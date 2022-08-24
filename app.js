f347b87ee735231f34da00ead1226f1b580f579b
Prathibha <prsthibha@gmail.com>
2022-08-24 12:30:01 +0530

    refactor code

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const date = require(__dirname + "/date.js");
+
 const app = express();
 
-let items = ["Buy food", "Cook food", "Eat food"];
-let workItems = [];
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -15,21 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
 app.get("/", (req, res) => {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-us", options);
-
+  const day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "work") {
     workItems.push(item);
@@ -43,12 +36,6 @@ app.post("/", function (req, res) {
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "work  List", newListItems: workItems });
 });
-
-// app.post('/work', function(req, res) {
-
-//   let item = req.body.newItem;
-//   res.redirect('/work');
-// });
 
 app.listen(port, function () {
   console.log("Listening on port " + port);
